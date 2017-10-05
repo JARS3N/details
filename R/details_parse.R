737d49c156d8de4086aff8b613914a9656d92e2f
JARS3N <JARS3N@users.noreply.github.com>
2017-10-05 14:37:28 -0400

    Create details_parse.R

---
+++ R/details_parse.R
@@ -0,0 +1,14 @@
+parse_<-function(doc){
+d<-xmlTreeParse(doc,useInternalNodes =T)
+if(check(d)){return("FAILURE")}
+barcode<-getBarcode(_barcoded)
+tbl<-find_table(d)
+html_tree<-XML::htmlTreeParse(tbl,useInternalNodes = T)
+tds<-xpathApply(html_tree, path = "//td")
+strs<-xmlApply(tds,getChildrenStrings,len=48)
+dfs<-do.call('rbind',lapply(strs,pull_cell_data))
+dfs$Lot<-paste0(substr(barcode,1,1),substr(barcode,7,11))
+dfs$sn<-substr(barcode,2,6)
+dfs$plat<-get_platform(substr(barcode,1,1))
+dfs[,c("Well","Lot","sn","plat" ,names(dfs)[1:20])]
+}
