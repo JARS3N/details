74e52e642600dd5f035f777883cec924de0aabb2
JARS3N <JARS3N@users.noreply.github.com>
2017-12-06 23:23:47 -0500

    Update details_parse.R

--- R/details_parse.R
+++ R/details_parse.R
@@ -1,14 +1,18 @@
-parse<-function(doc){
-d<-xmlTreeParse(doc,useInternalNodes =T)
-if(check_failure(d)){return("FAILURE")}
-barcode<-get_barcode(d)
-tbl<-find_table(d)
-html_tree<-XML::htmlTreeParse(tbl,useInternalNodes = T)
-tds<-xpathApply(html_tree, path = "//td")
-strs<-xmlApply(tds,getChildrenStrings,len=48)
-dfs<-do.call('rbind',lapply(strs,pull_cell_data))
-dfs$Lot<-paste0(substr(barcode,1,1),substr(barcode,7,11))
-dfs$sn<-substr(barcode,2,6)
-dfs$plat<-get_platform(substr(barcode,1,1))
-dfs[,c("Well","Lot","sn","plat" ,names(dfs)[1:20])]
+parse<-function (doc){
+  require(XML)
+  d <- xmlTreeParse(doc, useInternalNodes = T)
+  if (details::check_failure(d)) {
+    return("FAILURE")
+  }
+  barcode <- details::get_barcode(d)
+  tbl <- details::find_table(d)
+  html_tree <- XML::htmlTreeParse(tbl, useInternalNodes = T)
+  tds <- xpathApply(html_tree, path = "//td")
+  strs <- xmlApply(tds, getChildrenStrings, len = 48)
+  dfs <- do.call("rbind", lapply(strs, details::pull_cell_data))
+  dfs$Lot <- paste0(substr(barcode, 1, 1), substr(barcode, 
+                                                  7, 11))
+  dfs$sn <- substr(barcode, 2, 6)
+  dfs$plat <- details::get_platform(substr(barcode, 1, 1))
+  dfs[, c("Well", "Lot", "sn", "plat", names(dfs)[1:20])]
 }
