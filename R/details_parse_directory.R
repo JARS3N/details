607ec72a6557f16c45d8ee62c8978436fb0179f5
JARS3N <JARS3N@users.noreply.github.com>
2017-12-06 23:22:23 -0500

    Update details_parse_directory.R

--- R/details_parse_directory.R
+++ R/details_parse_directory.R
@@ -1,9 +1,11 @@
 parse_directory<-function(DIR=choose.dir()){
   require(parallel);
-  FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
+ # FLS<- list.files(DIR,recursive=T,pattern='details.xml',full.names = T);
+  # changed this because there always should be a file if there is a sub dir
+  FLS <- file.path(list.dirs(DIR,full.names = T,recursive = F),"details.xml")
   size.of.list <- length(FLS);
   cl <- makeCluster( min(size.of.list, detectCores()) );
-  work<-parallel::parLapply(cl=cl,FLS,details::parse)
+  work<-parallel::parLapply(cl=cl,FLS,parse)
   Out<-do.call('rbind',work[!grepl("FAILURE",work)])
   stopCluster(cl);
   Out
