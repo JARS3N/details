d2c647e8f5af5d6afd2f055c160c321aba467e83
JARS3N <JARS3N@users.noreply.github.com>
2017-12-15 12:15:30 -0500

    Update details_check_nightly.R

--- R/details_check_nightly.R
+++ R/details_check_nightly.R
@@ -1,22 +1,29 @@
-check_nightly<-function(need_to_add){
+check_nightly <- function(u) {
+  ignore_msg1 <- cat("nothing to see her,move along. ")
+  ignore_msg2 <- message(Sys.time())
   # check if anything to add
-  if(is.null(need_to_add) | nrow(need_to_add)<1){
-    cat("nothing to see her,move along. ")
-    message(Sys.time())
+  if (is.null(u)) {
+    ignore_msg1
+    ignore_msg2
     return(NULL)
-  }else{
-big_start <- Sys.time()
-BIG <- Map(details::parse_dir_with_msgs,
-           x = need_to_add$loc,
-           y = need_to_add$Lot)
-
-big_end <- Sys.time()
-big_delta <- big_end - big_start
-print(big_delta)
-suppressPackageStartupMessages(library(data.table))
-
-OUT<-data.table::rbindlist(BIG)
-
-details::write(as.data.frame(OUT))
-}
+  }
+  if (nrow(u) < 1) {
+    ignore_msg1
+    ignore_msg2
+    return(NULL)
+  } else{
+    big_start <- Sys.time()
+    BIG <- Map(details::parse_dir_with_msgs,
+               x = u$loc,
+               y = u$Lot)
+    
+    big_end <- Sys.time()
+    big_delta <- big_end - big_start
+    print(big_delta)
+    suppressPackageStartupMessages(library(data.table))
+    
+    OUT <- data.table::rbindlist(BIG)
+    
+    details::write(as.data.frame(OUT))
+  }
 }
