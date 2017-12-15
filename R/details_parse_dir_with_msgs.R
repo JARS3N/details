09258f2101c361631367a7b373926d8011974c5b
JARS3N <JARS3N@users.noreply.github.com>
2017-12-15 11:50:40 -0500

    Create details_parse_dir_with_msgs.R

---
+++ R/details_parse_dir_with_msgs.R
@@ -0,0 +1,10 @@
+parse_dir_with_msgs<-function(x,y){
+  start<-Sys.time()
+  df<-details::parse_directory(x)
+  end<-Sys.time()
+  delta_t<-end-start
+  cat("directory completed in ",delta_t," for Lot: ")
+  message(y)
+  cat("\n")
+  df
+}
