9499a02020a3680d53f97e33caf428de362cdab6
JARS3N <JARS3N@users.noreply.github.com>
2017-12-15 10:53:20 -0500

    Create details_check_db_presence.R

---
+++ R/details_check_db_presence.R
@@ -0,0 +1,9 @@
+check_db<-function(u){
+  conn<- adminKraken::con_mysql()
+  qry_str<-paste0('select DISTINCT(sn) from mvdata where Lot="',u,'";')
+  qry<-dbSendQuery(conn,qry_str)
+  qry_res<-dbFetch(qry)
+  dbDisconnect(conn)
+  qry_res$Lot<-u
+  qry_res
+}
