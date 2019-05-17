90068c5bf53447a919d974ac0fed0ba764aeb7ab
JARS3N <JARS3N@users.noreply.github.com>
2019-05-17 12:57:15 -0400

    Update details_parse_directory.R

--- R/details_parse_directory.R
+++ R/details_parse_directory.R
@@ -1,6 +1,6 @@
 parse_directory<-function(DIR=choose.dir()){
   require(parallel);
-  FLS <-list.files(path=DIR,recursive=T,full.names=T,pattern="details.xml")
+  FLS <-list.files(path=DIR,recursive=T,full.names=T,pattern="details.xml$")
   size.of.list <- length(FLS);
   cl <- makeCluster( min(size.of.list, detectCores()) );
   work<-parallel::parLapply(cl=cl,FLS,parse)
