3fda1c90aeaf16b7d05991605d78f0ca4d95f708
JARS3N <JARS3N@users.noreply.github.com>
2019-05-17 12:44:04 -0400

    Update details_parse_directory.R

--- R/details_parse_directory.R
+++ R/details_parse_directory.R
@@ -4,7 +4,7 @@ parse_directory<-function(DIR=choose.dir()){
   # changed this because there always should be a file if there is a sub dir
   #FLS <- file.path(list.dirs(DIR,full.names = T,recursive = F),"details.xml")
   # turns out there was a file missing and threw an error
-  FLS <- sapply(list.dirs(d,full.names = T,recursive = F),list.files,simplify=T,full.names=T,pattern="details.xml",USE.NAMES = F)
+  FLS <- sapply(list.dirs(DIR,full.names = T,recursive = F),list.files,simplify=T,full.names=T,pattern="details.xml",USE.NAMES = F)
   size.of.list <- length(FLS);
   cl <- makeCluster( min(size.of.list, detectCores()) );
   work<-parallel::parLapply(cl=cl,FLS,parse)
