1b3e90fbf1a07bd349372abf6e2c2893c25edc08
JARS3N <JARS3N@users.noreply.github.com>
2020-04-21 22:27:38 -0400

    Create alt_save.R

---
+++ R/alt_save.R
@@ -0,0 +1,12 @@
+alt_save <- function(j,target) {
+  lapply(j, function(u) {
+    df <- alt_parse_dir(u)
+    name <-   paste0(basename(dirname(u)),
+                     "_",
+                     basename(u),
+                     "_machinevision_",
+                     Sys.Date(),
+                     ".csv")
+    newpath <- file.path(target, name)
+    write.csv(df, newpath, row.names = F)
+}
