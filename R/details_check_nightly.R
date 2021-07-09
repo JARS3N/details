9ec6af54bca2dbb6a46391e89d4c4a9c0875f6b1
JARS3N <JARS3N@users.noreply.github.com>
2021-07-09 10:59:28 -0400

    Update details_check_nightly.R

--- R/details_check_nightly.R
+++ R/details_check_nightly.R
@@ -1,24 +1,24 @@
-check_nightly <- function(u) {
-  ignore_msg1 <- "nothing to see here,move along! "
-  ignore_msg2 <- Sys.time()
-  # check if anything to add
-  if (is.null(u)) {
-    cat(ignore_msg1)
-    cat(ignore_msg2)
-    return(NULL)
-  }
-  if (nrow(u) < 1) {
-    cat(ignore_msg1)
-    cat(ignore_msg2)
-    return(NULL)
-  } else{
-    big_start <- Sys.time()
-    BIG <- Map(details::parse_dir_with_msgs,
-               x = u$loc,
-               y = u$Lot)
-    
-    big_end <- Sys.time()
-    big_delta <- big_end - big_start
-    print(big_delta)
-  }
-}
+#check_nightly <- function(u) {
+#  ignore_msg1 <- "nothing to see here,move along! "
+#  ignore_msg2 <- Sys.time()
+#  # check if anything to add
+#  if (is.null(u)) {
+#    cat(ignore_msg1)
+#    cat(ignore_msg2)
+#    return(NULL)
+#  }
+#  if (nrow(u) < 1) {
+#    cat(ignore_msg1)
+#    cat(ignore_msg2)
+#    return(NULL)
+#  } else{
+#    big_start <- Sys.time()
+#    BIG <- Map(details::parse_dir_with_msgs,
+#               x = u$loc,
+#               y = u$Lot)
+#    
+#    big_end <- Sys.time()
+#    big_delta <- big_end - big_start
+#    print(big_delta)
+#  }
+#}
