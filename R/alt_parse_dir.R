ac65b69abe1594a4dd9733758c975fea16dac317
JARS3N <JARS3N@users.noreply.github.com>
2021-08-11 00:10:54 -0400

    Update alt_parse_dir.R

--- R/alt_parse_dir.R
+++ R/alt_parse_dir.R
@@ -3,7 +3,7 @@ alt_parse_dir<-function(DIR=choose.dir()){
   FLS <-list.files(path=DIR,recursive=T,full.names=T,pattern="details.xml$")
   size.of.list <- length(FLS);
   cl <- makeCluster( min(size.of.list, detectCores()) );
-  work<-parallel::parLapply(cl=cl,FLS,deltails::try_null)
+  work<-parallel::parLapply(cl=cl,FLS,details::try_null)
   Out<-dplyr::bind_rows(work)
   stopCluster(cl);
  Out
