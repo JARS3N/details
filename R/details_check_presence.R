461b58e4dcc0db254d1c4764a76ab166f6479e81
JARS3N <JARS3N@users.noreply.github.com>
2022-09-15 14:10:50 -0400

    Update details_check_presence.R

--- R/details_check_presence.R
+++ R/details_check_presence.R
@@ -1,9 +1,9 @@
 check_presence<-function(u){
-  conn<- adminKraken::con_mysql()
-  qry_str<-paste0('select DISTINCT(sn) from mvdata where Lot="',u,'";')
-  qry<-dbSendQuery(conn,qry_str)
-  qry_res<-dbFetch(qry)
-  dbDisconnect(conn)
-  qry_res$Lot<-u
-  qry_res
+ # conn<- adminKraken::con_mysql()
+  #qry_str<-paste0('select DISTINCT(sn) from mvdata where Lot="',u,'";')
+  #qry<-dbSendQuery(conn,qry_str)
+  #qry_res<-dbFetch(qry)
+  #dbDisconnect(conn)
+  #qry_res$Lot<-u
+  #qry_res
 }
