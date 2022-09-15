4a551f765cb41e5f4848440ff36cf4758759de8f
JARS3N <JARS3N@users.noreply.github.com>
2022-09-15 14:14:02 -0400

    Update details_cron.R

--- R/details_cron.R
+++ R/details_cron.R
@@ -1,9 +1,9 @@
-cron<-function(){
-print(Sys.time())
-df<-search_dirs()
-dbLots <- details::get_lots()
-need_to_add <- df[!(df$Lot %in% dbLots$Lot), ]
-need_to_add$nchar<-sapply(need_to_add$Lot,nchar)
-work<-details::check_nightly(need_to_add[need_to_add$nchar==6,])
-work
-}
+#cron<-function(){
+#print(Sys.time())
+#df<-search_dirs()
+#dbLots <- details::get_lots()
+#need_to_add <- df[!(df$Lot %in% dbLots$Lot), ]
+#need_to_add$nchar<-sapply(need_to_add$Lot,nchar)
+#work<-details::check_nightly(need_to_add[need_to_add$nchar==6,])
+#work
+#}
