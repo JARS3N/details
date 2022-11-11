24756f3eebfdc00959156211310a9e75be27457b
JARS3N <JARS3N@users.noreply.github.com>
2022-11-11 15:04:36 -0500

    Update details_find_table.R

--- R/details_find_table.R
+++ R/details_find_table.R
@@ -1,3 +1,3 @@
-find_table<-function(d){
-  xpathSApply(d, path = "//List//InspectionDetailsItem[Name='Results']//Details",xmlValue)
-}
+#find_table<-function(d){
+#  xpathSApply(d, path = "//List//InspectionDetailsItem[Name='Results']//Details",xmlValue)
+#}
